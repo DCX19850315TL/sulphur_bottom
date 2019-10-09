22520f584d0b0a76630721a2cc34fa1ca5934e77
tangl <tanglei_0315@163.com>
2019-10-09 10:59:54 +0800

    创建第一个应用，创建数据库并修改完配置后的代码提交。

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -19,10 +19,10 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': '',                            # 你的数据库user
-        'PASSWORD': '',                        # 你的数据库password
-        'HOST': '',                   		   # 数据库HOST
+        'NAME': 'classdbprod',                        # 数据库名 (默认与APP_ID相同)
+        'USER': 'classdbprod',                            # 你的数据库user
+        'PASSWORD': 'classdbprod@2019',                        # 你的数据库password
+        'HOST': '192.168.10.112',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
